d679bd270a70f5209ac37a1e582a9f6bd0422d28
odjhey <odjhey@gmail.com>
2024-04-15 18:28:54 +0800

    chore: update eslint for bff

--- bff/src/services/todo/todo.services.ts
+++ bff/src/services/todo/todo.services.ts
@@ -5,20 +5,27 @@ import { TRPCError } from '@trpc/server'
 let todos: string[] = []
 
 export const todoRoute = router({
-    todos: publicProcedure
-        .query(() => {
-            return todos
-        }),
+  todos: publicProcedure.query(() => {
+    return todos
+  }),
 
-        sync: publicProcedure.input(z.object({todos: z.array(z.string())})).mutation(({input: {todos : todosInput}}) => {
-            todos = todosInput
-        }),
+  sync: publicProcedure
+    .input(z.object({ todos: z.array(z.string()) }))
+    .mutation(({ input: { todos: todosInput } }) => {
+      todos = todosInput
+    }),
 
-
-    new: publicProcedure
-        .input(z.object(
-            { todo: z.string().min(1) }
-        )).mutation(({ input: { todo } }) => {
-            todos.push(todo)
+  new: publicProcedure
+    .input(z.object({ todo: z.string().min(1) }))
+    .mutation(({ input: { todo } }) => {
+      const bannedWords = ['lol', 'foo', 'bar']
+      if (bannedWords.includes(todo)) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'Word is banned',
         })
+      }
+
+      todos.push(todo)
+    }),
 })
